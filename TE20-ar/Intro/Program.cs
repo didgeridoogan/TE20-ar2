f447fa2b0c709a92bd16c4274a04fb5fe0241a03
DILARA DOGAN <dogan.dilara@elev.ga.ntig.se>
2021-09-08 11:07:10 +0200

    Tränat på cw och specialtecken

---
+++ TE20-ar/Intro/Program.cs
@@ -0,0 +1,19 @@
+﻿using Internal;
+using System;
+
+namespace Intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Mitt namn är Dilara Dogan");
+            Console.WriteLine("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." + "\nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." + "\nExcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+
+            Console.WriteLine("I prog 1 läser vi C#");
+
+            Console.WriteLine("Mitt spelnick är \"xXEpisk Gamer420Xx\"");
+        }
+    }
+}
---
+++ TE20-ar/Uppgift1.2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Uppgift1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Detta är ett program gjort i C#." + 
+            "\nProgrammet skriver ut text på flera rader." +
+            "\nDetta är programmets sista utskrift.");
+        }
+    }
+}
