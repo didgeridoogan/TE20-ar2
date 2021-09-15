5149dc2b061821ec31099522a44bc8db5ceafff4
DILARA DOGAN <dogan.dilara@elev.ga.ntig.se>
2021-09-15 11:07:55 +0200

    Lucktext

--- TE20-ar/Kapitel 2/Lucktext/Program.cs
+++ TE20-ar/Kapitel 2/Lucktext/Program.cs
@@ -1,5 +1,4 @@
-﻿using Internal;
-using System;
+﻿using System;
 
 namespace Lucktext
 {
@@ -8,11 +7,23 @@ namespace Lucktext
         static void Main(string[] args)
         {
             Console.Clear();
-            Console.Write("Lägg in dina ordval");
 
-            Console.Write("Adjektiv ");
+            Console.WriteLine("Lägg in dina ordval");
 
+            Console.WriteLine("Skriv in ett adjektiv: ");
+            string adjektiv = Console.ReadLine();
 
+            Console.WriteLine("Plats ");
+            string plats = Console.ReadLine();
+
+            Console.WriteLine("Relation ");
+            string relation = Console.ReadLine();
+
+            Console.WriteLine("Substantiv ");
+            string substantiv = Console.ReadLine();
+
+            Console.WriteLine("Namn ");
+            string namn = Console.ReadLine();
 
             Console.WriteLine($"Det fanns en gång en {adjektiv} hund. Denna hund var på väg till {plats} för att träffa sin {relation} när han plötsligt såg en {substantiv}. {substantiv} använde sina laserögon för att spränga hunden, som förresten hette {namn}")
         }
