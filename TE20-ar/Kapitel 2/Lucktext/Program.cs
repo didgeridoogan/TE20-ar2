66323131319dba54663cdc2239af5a3051bfb30d
DILARA DOGAN <dogan.dilara@elev.ga.ntig.se>
2021-09-15 10:54:38 +0200

    Lucktext

---
+++ TE20-ar/Kapitel 2/Lucktext/Program.cs
@@ -0,0 +1,20 @@
+﻿using Internal;
+using System;
+
+namespace Lucktext
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Write("Lägg in dina ordval");
+
+            Console.Write("Adjektiv ");
+
+
+
+            Console.WriteLine($"Det fanns en gång en {adjektiv} hund. Denna hund var på väg till {plats} för att träffa sin {relation} när han plötsligt såg en {substantiv}. {substantiv} använde sina laserögon för att spränga hunden, som förresten hette {namn}")
+        }
+    }
+}
