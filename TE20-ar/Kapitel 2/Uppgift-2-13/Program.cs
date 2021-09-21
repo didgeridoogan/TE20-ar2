6789e703b4c814bb09d9b0fc2297e7a541686e66
DILARA DOGAN <dogan.dilara@elev.ga.ntig.se>
2021-09-21 11:05:00 +0200

    parse övningar

---
+++ TE20-ar/Kapitel 2/Uppgift-2-13/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lön
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ange löner
+            Console.WriteLine("Skriv in månadslöner: " );
+            int lön1 = int.Parse(Console.ReadLine());
+            int lön2 = int.Parse(Console.ReadLine());
+            int lön3 = int.Parse(Console.ReadLine());
+
+            //Rökna ut medelvärdet
+            float medelvärde = (lön1 + lön2 + lön3) / 3;
+
+            //Skriv ut resultatet
+            Console.Write($"Medellönen för alla är {medelvärde}");
+
+        }
+    }
+}
