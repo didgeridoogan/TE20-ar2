c8d12dea29fb47cb05e763d659dbb3e4b38df3cf
DILARA DOGAN <dogan.dilara@elev.ga.ntig.se>
2021-09-22 11:08:16 +0200

    Tränat på if-satser och omvandla små bokstäver

---
+++ TE20-ar/Kapitel 3/Interaktiv-Berättelse/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Interaktiv_Berättelse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Du är en fet liten råtta vid namn Kim. Du måste snabbt fly hem innan det blir morgon så att du kan sno din husägares ost. Mat är väldigt viktigt för dig, förstår du.");
+            Console.WriteLine("Först, väljer du att gå åt höger eller vänster?");
+            string riktning = Console.ReadLine();
+            
+            if (riktning == "höger")
+            {
+             Console.WriteLine("Du stötte på katten Gustav, han åt upp dig för han trodde du var lasagne :( Du tror att du är död, men du lever faktiskt inuti katten! Detta betyder att du lätt och snabbt kan bryta dig in i huset nu medans du gömmer dig i Gustav. Bra gjort, fortsätt...");  
+             Console.WriteLine("Du lyckades till slut komma ur katten och är nu inuti köket. Väljer du att leta efter osten i kylskåpet eller på bordet?"); 
+             if ()
+             {
+                 
+             }
+             else
+             {
+                 
+             }
+            }
+
+
+        }
+    }
+}
---
+++ TE20-ar/Kapitel 3/Villkor/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Villkor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Be användaren om ålder
+            Console.Write("Hur gammal är du? (heltal)");
+            int ålder = int.Parse(Console.ReadLine());
+
+            // Om ålder större än 18 "Du får ta körkort"
+            if (ålder >= 18)
+            {
+                Console.WriteLine("Du får ta körkort!");
+            }
+
+            // Om ålder är 15 eller högre -> "Du får ta mopedkörkort!"
+            if (ålder >= 15)
+            {
+                Console.WriteLine("Du får ta mopedkörkort!");
+            }
+
+            // Fråga användaren "Vad heter ABBAs senaste album?"
+            Console.WriteLine("Vad heter ABBAs senaste album?");
+            string album = Console.ReadLine();
+            
+            // Är svaret korrekt?
+            if (album == "Voyage" || album == "voyage")
+            {
+                Console.WriteLine("Bra svarat!");
+            }
+            else
+            {
+                Console.WriteLine("Fel svar! Albumet heter Voyage...");
+            }
+
+            // Sista TP-fråga
+            Console.Write("Vem missade straffet i matchen England V Frankrike? (efternamn)");
+            
+            // Läs in och tvinga till små bokstäver. Mbappe -> mbappe/ mBappe -> mbappe
+            string spelare = Console.ReadLine().ToLower();
+            
+            if (spelare == "Mbappe")
+            {
+                Console.WriteLine("Bra, du är en expert!");
+            }
+            else
+            {
+                Console.WriteLine("Dåligt, du är sämst! Det var Mbappe");
+            }
+
+        }
+    }
+}
