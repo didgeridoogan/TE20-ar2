2d42d809fde030dc95ed60558815883f7b70dfdc
DILARA DOGAN <dogan.dilara@elev.ga.ntig.se>
2021-12-08 10:01:16 +0100

    lol

--- TE20-ar/Kapitel-5/fleraDimensioner/Program.cs
+++ TE20-ar/Kapitel-5/fleraDimensioner/Program.cs
@@ -7,6 +7,17 @@ namespace fleraDimensioner
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            int[,] rutor = new int[2, 3];
+
+            rutor [0,0] = 1;
+            rutor [1,0] = 1;
+            rutor [0,1] = 1;
+            rutor [1,1] = 1;
+            rutor [0,2] = 1;
+            rutor [0,2] = 1;
+            rutor [1,2] = 1;
+
         }
     }
 }
